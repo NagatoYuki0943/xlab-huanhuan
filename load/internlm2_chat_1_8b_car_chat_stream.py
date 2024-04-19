02300a4182a80440d6524b58f5ce2ccdab94bfd8
yuki <lihongtu12138@outlook.com>
2024-04-19 17:26:46 +0800

    add car train

---
+++ load/internlm2_chat_1_8b_car_chat_stream.py
@@ -0,0 +1,88 @@
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import PeftModel
+import torch
+
+
+print("torch version: ", torch.__version__)
+print("transformers version: ", transformers.__version__)
+
+
+model_path = "./models/internlm2-chat-1_8b"
+adapter_dir = "./work_dirs/internlm2_chat_1_8b_qlora_car_e20/hf"
+
+# 量化
+quantization = False
+
+# tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
+
+# 量化
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,                      # 是否在4位精度下加载模型。如果设置为True，则在4位精度下加载模型。
+    load_in_8bit=False,
+    llm_int8_threshold=6.0,
+    llm_int8_has_fp16_weight=False,
+    bnb_4bit_compute_dtype=torch.float16,   # 4位精度计算的数据类型。这里设置为torch.float16，表示使用半精度浮点数。
+    bnb_4bit_quant_type='nf4',              # 4位精度量化的类型。这里设置为"nf4"，表示使用nf4量化类型。 nf4: 4bit-NormalFloat
+    bnb_4bit_use_double_quant=True,         # 是否使用双精度量化。如果设置为True，则使用双精度量化。
+)
+
+# 创建模型
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype=torch.float16,
+    trust_remote_code=True,
+    device_map='auto',
+    low_cpu_mem_usage=True, # 是否使用低CPU内存,使用 device_map 参数必须为 True
+    quantization_config=quantization_config if quantization else None,
+)
+model.eval()
+
+# 2种加载adapter的方式
+# 1. load adapter https://huggingface.co/docs/transformers/main/zh/peft
+# model.load_adapter(adapter_dir)
+
+# 2. https://huggingface.co/docs/peft/v0.9.0/en/package_reference/peft_model#peft.PeftModel.from_pretrained
+model = PeftModel.from_pretrained(model, adapter_dir)
+
+# print(model.__class__.__name__) # InternLM2ForCausalLM
+
+print(f"model.device: {model.device}, model.dtype: {model.dtype}")
+
+system_prompt = "你现在是评论总结小助手，负责总结用户对汽车的评论，要按照以下顺序输出评论的各个内容\n" + \
+    "1、首先输出这个车的整体评价，要求言简意赅。\n" + \
+    "2、然后输出这个车的好的评价，要求言简意赅。\n" + \
+    "3、之后输出这个车的不好的评价，要求言简意赅。\n" + \
+    "4、最后输出这个车的每个部分的评价，有多少提取多少。\n" + \
+    "注意，只总结用户的输入的信息，不要自己编造用户没说的信息，一下是用户的评论，请进行总结\n"
+print(system_prompt)
+
+
+while True:
+    query = input("请输入提示: ")
+    query = query.replace(' ', '')
+    if query == None or len(query) < 1:
+        continue
+    if query.lower() == "exit":
+        break
+
+    print("回答: ", end="")
+    # https://huggingface.co/internlm/internlm2-chat-1_8b/blob/main/modeling_internlm2.py#L1185
+    # stream_chat 返回的句子长度是逐渐边长的,length的作用是记录之前的输出长度,用来截断之前的输出
+    length = 0
+    
+    for response, history in model.stream_chat(
+            tokenizer = tokenizer,
+            query = query,
+            history = history,
+            max_new_tokens = 1024,
+            do_sample = True,
+            temperature = 0.8,
+            top_p = 0.8,
+            meta_instruction = system_prompt,
+        ):
+        if response is not None:
+            print(response[length:], flush=True, end="")
+            length = len(response)
+    print("\n")
