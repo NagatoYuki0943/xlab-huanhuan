d360daed42d9c69c13c994435fc1d13e49f75e7f
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-04-21 16:28:22 +0800

    add huatuo stream

---
+++ load/internlm2_chat_1_8b_huatuo_chat_stream.py
@@ -0,0 +1,47 @@
+from load_model import load_model
+
+
+# clone 模型
+pretrained_model_name_or_path = "./internlm2-chat-1_8b"
+# os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-1.8b {pretrained_model_name_or_path}')
+# os.system(f'cd {pretrained_model_name_or_path} && git lfs pull')
+adapter_dir = "./internlm2_chat_1_8b_qlora_huatuo_e3/epoch_3_hf"
+
+# 量化
+load_in_8bit = False
+load_in_4bit = False
+
+tokenizer, model = load_model(pretrained_model_name_or_path, adapter_dir, load_in_8bit, load_in_4bit)
+
+system_prompt = "你现在是一名医生，具备丰富的医学知识和临床经验。你擅长诊断和治疗各种疾病，能为病人提供专业的医疗建议。你有良好的沟通技巧，能与病人和他们的家人建立信任关系。请在这个角色下为我解答以下问题。"
+print(system_prompt)
+
+
+history = []
+while True:
+    query = input("请输入提示: ")   # ex: 膝盖位置在天冷的时候玩不下去怎么办
+    query = query.replace(' ', '')
+    if query == None or len(query) < 1:
+        continue
+    if query.lower() == "exit":
+        break
+
+    print("回答: ", end="")
+    # https://huggingface.co/internlm/internlm2-chat-1_8b/blob/main/modeling_internlm2.py#L1185
+    # stream_chat 返回的句子长度是逐渐边长的,length的作用是记录之前的输出长度,用来截断之前的输出
+    length = 0
+    for response, history in model.stream_chat(
+            tokenizer = tokenizer,
+            query = query,
+            history = history,
+            max_new_tokens = 1024,
+            do_sample = True,
+            temperature = 0.8,  # 温度设置的很低，保证输出更准确
+            top_p = 0.8,
+            top_k = 40,
+            meta_instruction = system_prompt,
+        ):
+        if response is not None:
+            print(response[length:], flush=True, end="")
+            length = len(response)
+    print("\n")
