82f37e976e01121e0c1f21380699cd18842dd4db
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-04-29 06:10:22 +0800

    chatbox add placeholder

--- lmdeploy/turbomind_gradio_stream.py
+++ lmdeploy/turbomind_gradio_stream.py
@@ -187,7 +187,7 @@ def main():
             )
 
         gr.Markdown("""提醒：<br>
-        1. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。<br>
+        1. 内容由 AI 大模型生成，请仔细甄别。<br>
         """)
 
     # threads to consume the request
