33db0e2ec2d9857f23d3c99fd65988fdb560e01d
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-05-13 20:04:55 +0800

    add uuid func

---
+++ load/utils.py
@@ -0,0 +1,20 @@
+import uuid
+from typing import Literal
+
+
+def random_uuid(dtype: Literal['int', 'str', 'bytes', 'time'] = 'int') -> int | str | bytes:
+    """生成随机uuid
+    reference: https://github.com/vllm-project/vllm/blob/main/vllm/utils.py
+    """
+    assert dtype in ['int', 'str', 'bytes', 'time'], f"unsupported dtype: {dtype}, should be in ['int', 'str', 'bytes', 'time']"
+
+    # uuid4: 由伪随机数得到，有一定的重复概率，该概率可以计算出来。
+    uid = uuid.uuid4()
+    if dtype == 'int':
+        return uid.int
+    elif dtype == 'str':
+        return uid.hex
+    elif dtype == 'bytes':
+        return uid.bytes
+    else:
+        return uid.time
