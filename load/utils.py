73988f6d68ae0d1a1b53cdeca7be0d4ee324f11e
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-05-21 20:21:01 +0800

    修复lmdeploy随机数bug

--- load/utils.py
+++ load/utils.py
@@ -1,5 +1,6 @@
 import uuid
 from typing import Literal
+import random
 
 
 def random_uuid(dtype: Literal['int', 'str', 'bytes', 'time'] = 'int') -> int | str | bytes:
@@ -18,3 +19,13 @@ def random_uuid(dtype: Literal['int', 'str', 'bytes', 'time'] = 'int') -> int | 
         return uid.bytes
     else:
         return uid.time
+
+
+def random_uuid_int() -> int:
+    """random_uuid 生成的 int uuid 会超出int64的范围,lmdeploy使用会报错"""
+    return random.getrandbits(64)
+
+
+if __name__ == '__main__':
+    print(random_uuid('int'))
+    print(random_uuid_int())
