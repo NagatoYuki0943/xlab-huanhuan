0372f9fa06a2e6baeac0aaaf09296376b3dff3d8
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-05-29 20:58:57 +0800

    支持 api model

---
+++ load/infer_engine_api_chat_stream.py
@@ -0,0 +1,48 @@
+from infer_engine import InferEngine, ApiConfig
+import os
+
+
+"""
+设置临时变量
+
+linux:
+    export API_KEY="your token"
+
+powershell:
+    $env:API_KEY = "your token"
+"""
+api_config = ApiConfig(
+    base_url = "https://api.moonshot.cn/v1",
+    api_key = os.getenv("API_KEY", "I AM AN API_KEY"),
+    model = "moonshot-v1-8k",
+)
+
+
+infer_engine = InferEngine(backend='api', api_config=api_config)
+print(infer_engine.get_available_models())
+# ['moonshot-v1-8k', 'moonshot-v1-32k', 'moonshot-v1-128k']
+
+
+history = [] # [['What is the capital of France?', 'The capital of France is Paris.'], ['Thanks', 'You are Welcome']]
+while True:
+    query = input("请输入提示: ")
+    query = query.strip()
+    if query == None or len(query) < 1:
+        continue
+    if query.lower() == "exit":
+        break
+
+    print(f"回答: ", end="", flush=True)
+    length = 0
+    for response, history in infer_engine.chat_stream(
+        query = query,
+        history = history,
+        max_new_tokens = 1024,
+        temperature = 0.8,
+        top_p = 0.8,
+        top_k = 40,
+        model = "moonshot-v1-8k",
+    ):
+        print(response[length:], flush=True, end="")
+        length = len(response)
+    print("\n回答: ", response)
