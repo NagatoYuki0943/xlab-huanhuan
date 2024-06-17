8fcb54cc23775907526cd3806a78d2aa4e1de188
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-06-17 08:28:13 +0800

    add llama3 chat

---
+++ load/Meta-Llama-3-Instruct_chat.py
@@ -0,0 +1,46 @@
+import os
+from infer_engine import InferEngine, TransformersConfig
+
+
+# clone 模型
+PRETRAINED_MODEL_NAME_OR_PATH = '../models/Meta-Llama-3-8B-Instruct'
+ADAPTER_PATH = None
+# 量化
+LOAD_IN_8BIT= False
+LOAD_IN_4BIT = False
+
+SYSTEM_PROMPT = """You are a helpful, respectful and honest assistant."""
+
+TRANSFORMERS_CONFIG = TransformersConfig(
+    pretrained_model_name_or_path = PRETRAINED_MODEL_NAME_OR_PATH,
+    adapter_path = ADAPTER_PATH,
+    load_in_8bit = LOAD_IN_8BIT,
+    load_in_4bit = LOAD_IN_4BIT,
+    model_name = 'internlm2',
+    system_prompt = SYSTEM_PROMPT
+)
+
+# 载入模型
+infer_engine = InferEngine(
+    backend = 'transformers', # transformers, lmdeploy
+    transformers_config = TRANSFORMERS_CONFIG,
+)
+
+history = [] # [['What is the capital of France?', 'The capital of France is Paris.'], ['Thanks', 'You are Welcome']]
+while True:
+    query = input("请输入提示: ")
+    query = query.strip()
+    if query == None or len(query) < 1:
+        continue
+    if query.lower() == "exit":
+        break
+
+    response, history = infer_engine.chat(
+        query = query,
+        history = history,
+        max_new_tokens = 1024,
+        temperature = 0.8,
+        top_p = 0.8,
+        top_k = 40,
+    )
+    print("回答:", response)
