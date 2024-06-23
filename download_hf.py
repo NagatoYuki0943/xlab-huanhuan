7538729b635139de92c9689ff90b5532569efc6c
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-06-23 20:53:32 +0800

    add download_hf.py

---
+++ download_hf.py
@@ -0,0 +1,28 @@
+import os
+from huggingface_hub import hf_hub_download, snapshot_download
+
+
+"""
+设置临时变量
+
+linux:
+    export HF_TOKEN="your token"
+
+powershell:
+    $env:HF_TOKEN = "your token"
+
+"""
+hf_token = os.getenv("HF_TOKEN", "")
+
+endpoint = "https://hf-mirror.com"
+proxies = {"https": "http://localhost:7897"}
+
+
+snapshot_download(
+    repo_id = "maidalun1020/bce-embedding-base_v1",
+    local_dir = "./models/internlm2-chat-1_8b",
+    # proxies = proxies,
+    max_workers = 8,
+    # endpoint = endpoint,
+    # token = hf_token
+)
