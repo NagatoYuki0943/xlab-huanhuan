c76fc0d3d3f9f9124aa2bf706d14dd16fbd9e542
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-08-18 17:54:59 +0800

    add internvl2 train config

---
+++ train/internvl_v2_internlm2_2b_qlora_finetune_e1.py
@@ -0,0 +1,238 @@
+"""
+train:
+    xtuner train $CONFIG [other_config]
+    ex:
+        xtuner train train/internvl_v2_internlm2_2b_qlora_finetune_e1.py --deepspeed deepspeed_zero2
+
+convert:
+    xtuner convert pth_to_hf $CONFIG $PATH_TO_PTH_MODEL $SAVE_PATH_TO_HF_MODEL --max-shard-size 2GB
+
+    ex:
+        xtuner convert pth_to_hf \
+            train/internvl_v2_internlm2_2b_qlora_finetune_e1.py \
+            work_dirs/internvl_v2_internlm2_2b_qlora_finetune_e1/epoch_1.pth \
+            work_dirs/internvl_v2_internlm2_2b_qlora_finetune_e1/epoch_1.hf \
+            --max-shard-size 2GB
+
+merge adapter:
+    xtuner convert merge $LLM $ADAPTER $SAVE_PATH --max-shard-size 2GB
+
+    ex:
+        xtuner convert merge \
+            models/InternVL2-2B \
+            work_dirs/internvl_v2_internlm2_2b_qlora_finetune_e1/epoch_1.hf \
+            work_dirs/internvl_v2_internlm2_2b_qlora_finetune_e1/epoch_1_merged \
+            --max-shard-size 2GB
+
+chat:
+    xtuner chat $LLM --adapter $ADAPTER --bits $BITS --temperature $TEMPERATURE --top-k $TOP_K --top-p $TOP_P --system $SYSTEM_TEXT
+
+    ex:
+        xtuner chat \
+            models/InternVL2-2B \
+            --adapter work_dirs/internvl_v2_internlm2_2b_qlora_finetune_e1/epoch_1.hf \
+            --bits 8 --temperature 0.7 --top-k 50 --top-p 0.9 \
+
+验证数据集是否正确构建:
+    xtuner check-custom-dataset $CONFIG
+
+    ex:
+        xtuner check-custom-dataset train/internvl_v2_internlm2_2b_qlora_finetune_e1.py
+"""
+
+
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmengine.hooks import (CheckpointHook, DistSamplerSeedHook, IterTimerHook,
+                            LoggerHook, ParamSchedulerHook)
+from mmengine.visualization import Visualizer, LocalVisBackend, TensorboardVisBackend
+from mmengine.optim import AmpOptimWrapper, CosineAnnealingLR, LinearLR
+from torch.optim import AdamW
+from transformers import AutoTokenizer
+from peft import LoraConfig, TaskType
+
+from xtuner.dataset import InternVL_V1_5_Dataset
+from xtuner.dataset.collate_fns import default_collate_fn
+from xtuner.dataset.samplers import LengthGroupedSampler
+from xtuner.engine.hooks import (DatasetInfoHook, EvaluateChatHook,
+                                 ThroughputHook)
+from xtuner.engine.runner import TrainLoop
+from xtuner.model import InternVL_V1_5
+from xtuner.utils import PROMPT_TEMPLATE
+
+#######################################################################
+#                          PART 1  Settings                           #
+#######################################################################
+# Model
+model_path = './models/InternVL2-2B'
+
+# Data
+data_root = './datasets/llava_data/'
+data_path = data_root + 'LLaVA-Instruct-150K/llava_v1_5_mix665k.json'
+image_folder = data_root + 'llava_images'
+prompt_template = PROMPT_TEMPLATE.internlm2_chat
+max_length = 8192
+
+# Scheduler & Optimizer
+batch_size = 8  # per_device
+accumulative_counts = 2
+dataloader_num_workers = 4
+max_epochs = 1
+optim_type = AdamW
+# official 1024 -> 4e-5
+lr = 1e-6
+betas = (0.9, 0.999)
+weight_decay = 0.05
+max_norm = 1  # grad clip
+warmup_ratio = 0.03
+
+# Save
+by_epoch = False    # save and log by epoch or by iteration
+save_steps = 1000
+save_total_limit = 1  # Maximum checkpoints to keep (-1 means unlimited)
+
+#######################################################################
+#            PART 2  Model & Tokenizer & Image Processor              #
+#######################################################################
+model = dict(
+    type=InternVL_V1_5,
+    model_path=model_path,
+    freeze_llm=True,
+    freeze_visual_encoder=True,
+    quantization_llm=True,   # or False
+    quantization_vit=False,  # or True and uncomment visual_encoder_lora
+    # comment the following lines if you don't want to use Lora in llm
+    llm_lora=dict(
+        type=LoraConfig,
+        task_type=TaskType.CAUSAL_LM,
+        r=128,
+        lora_alpha=256,
+        lora_dropout=0.05,
+        target_modules=None),
+    # uncomment the following lines if you don't want to use Lora in visual encoder # noqa
+    # visual_encoder_lora=dict(
+    #     type=LoraConfig, r=64, lora_alpha=16, lora_dropout=0.05,
+    #     target_modules=['attn.qkv', 'attn.proj', 'mlp.fc1', 'mlp.fc2'])
+)
+
+#######################################################################
+#                      PART 3  Dataset & Dataloader                   #
+#######################################################################
+llava_dataset = dict(
+    type=InternVL_V1_5_Dataset,
+    model_path=model_path,
+    data_paths=data_path,
+    image_folders=image_folder,
+    template=prompt_template,
+    max_length=max_length)
+
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=dataloader_num_workers,
+    dataset=llava_dataset,
+    sampler=dict(
+        type=LengthGroupedSampler,
+        length_property='modality_length',
+        per_device_batch_size=batch_size * accumulative_counts),
+    collate_fn=dict(type=default_collate_fn))
+
+#######################################################################
+#                    PART 4  Scheduler & Optimizer                    #
+#######################################################################
+# optimizer
+optim_wrapper = dict(
+    type=AmpOptimWrapper,
+    optimizer=dict(
+        type=optim_type, lr=lr, betas=betas, weight_decay=weight_decay),
+    clip_grad=dict(max_norm=max_norm, error_if_nonfinite=False),
+    accumulative_counts=accumulative_counts,
+    loss_scale='dynamic',
+    dtype='float16')
+
+# learning policy
+# More information: https://github.com/open-mmlab/mmengine/blob/main/docs/en/tutorials/param_scheduler.md  # noqa: E501
+param_scheduler = [
+    dict(
+        type=LinearLR,
+        start_factor=1e-5,
+        by_epoch=True,
+        begin=0,
+        end=warmup_ratio * max_epochs,
+        convert_to_iter_based=True),
+    dict(
+        type=CosineAnnealingLR,
+        eta_min=0.0,
+        by_epoch=True,
+        begin=warmup_ratio * max_epochs,
+        end=max_epochs,
+        convert_to_iter_based=True)
+]
+
+# train, val, test setting
+# 在 EpochBased 模式下，val_interval 的默认值为 1，表示训练一个 Epoch，验证一次
+# 在 IterBased 模式下，val_interval 的默认值为 1000，表示训练迭代 1000 次，验证一次
+# train_cfg = dict(type=TrainLoop, max_epochs=max_epochs)
+train_cfg = dict(by_epoch=True, max_epochs=max_epochs, val_interval=1)
+
+#######################################################################
+#                           PART 5  Runtime                           #
+#######################################################################
+# Log the dialogue periodically during the training process, optional
+tokenizer = dict(
+    type=AutoTokenizer.from_pretrained,
+    pretrained_model_name_or_path=model_path,
+    trust_remote_code=True)
+
+custom_hooks = [
+    dict(type=DatasetInfoHook, tokenizer=tokenizer),
+    # dict(type=ThroughputHook)
+]
+
+# configure default hooks
+default_hooks = dict(
+    # record the time of every iteration.
+    timer=dict(type=IterTimerHook),
+    # print log every 10 iterations.
+    logger=dict(type=LoggerHook, log_metric_by_epoch=False, interval=10),
+    # enable the parameter scheduler.
+    param_scheduler=dict(type=ParamSchedulerHook),
+    # save checkpoint per `save_steps`.
+    checkpoint=dict(
+        type=CheckpointHook,
+        save_optimizer=False,
+        by_epoch=by_epoch,
+        interval=save_steps,
+        max_keep_ckpts=save_total_limit),
+    # set sampler seed in distributed evrionment.
+    sampler_seed=dict(type=DistSamplerSeedHook),
+)
+
+# configure environment
+env_cfg = dict(
+    # whether to enable cudnn benchmark
+    cudnn_benchmark=False,
+    # set multi process parameters
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    # set distributed parameters
+    dist_cfg=dict(backend='nccl'),
+)
+
+# set visualizer
+visualizer = dict(
+    type=Visualizer,
+    vis_backends=[dict(type=LocalVisBackend), dict(type=TensorboardVisBackend)]
+)
+
+# set log level
+log_level = 'INFO'
+
+# load from which checkpoint
+load_from = None
+
+# whether to resume training from the loaded checkpoint
+resume = False
+
+# Defaults to use random seed and disable `deterministic`
+randomness = dict(seed=None, deterministic=False)
+
+# set log processor
+log_processor = dict(by_epoch=by_epoch)
