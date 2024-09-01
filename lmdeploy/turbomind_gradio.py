5c526f54e04523e1f8b0a8ed66650f873dd126a1
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-09-01 20:54:04 +0800

    127.0.0.1 -> 0.0.0.0

--- lmdeploy/turbomind_gradio.py
+++ lmdeploy/turbomind_gradio.py
@@ -287,7 +287,7 @@ def main():
     )
 
     demo.launch(
-        server_name = "127.0.0.1",
+        server_name = "0.0.0.0",
         server_port = 7860,
         share = True,
         max_threads = 100,
